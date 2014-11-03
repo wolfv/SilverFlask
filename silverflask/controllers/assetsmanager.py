1bfadb1c9121f2e0438d33e5f8b0ddf7b1ff9f00
Wolf Vollprecht <w.vollprecht@gmail.com>
2014-11-03 16:49:55 +0100

    added some missing files

--- silverflask/controllers/assetsmanager.py
+++ silverflask/controllers/assetsmanager.py
@@ -1,22 +1,35 @@
 from .cms import bp
 from silverflask.models import FileObject
 from flask_wtf import Form
-from flask import render_template, jsonify, url_for
+from flask import render_template, jsonify, url_for, abort, request
 from silverflask.fields import GridField
 from silverflask import db
 
-@bp.route("/assets/edit/<int:record_id>")
+url_prefix = "/assets"
+
+@bp.route("/assets/edit/<int:record_id>", methods=["POST", "GET"])
 def assets_edit(record_id):
-    pass
+    f = db.session.query(FileObject).get(record_id)
+    if not f:
+        abort("Not found", 404)
+    form_class = FileObject.get_cms_form()
+    form = form_class(request.form, obj=f)
+    if form.validate_on_submit():
+        form.populate_obj(f)
+        db.session.commit()
+        return "Succeeded"
+
+    return render_template("assetmanager/edit.html", elem=f, page_form=form)
 
 @bp.route("/assets/gridfield")
 def assets_get():
     q = FileObject.query.all()
-    def get_return_dict(elem):
-        return elem.as_dict().update({
-            "edit_url": url_for(".assets_edit", record_id=elem.id)
-        })
-    return jsonify(data=[get_return_dict(r) for r in q])
+    res = []
+    for r in q:
+        d = r.as_dict()
+        d.update({"edit_url": url_for(".assets_edit", record_id=r.id)})
+        res.append(d)
+    return jsonify(data=res)
 
 
 @bp.route("/assets")
--- silverflask/models/FileObject.py
+++ silverflask/models/FileObject.py
@@ -1,12 +1,18 @@
 from .DataObject import DataObject
 import os
 from werkzeug.datastructures import FileStorage
-
+from wtforms import fields
 from silverflask import db
-
+from PIL import Image
+import uuid
+import errno
+import shutil
 
 class FileStorageBackend(object):
     def store(self, filepointer, location):
+        raise NotImplementedError()
+
+    def exists(self, location):
         raise NotImplementedError()
 
     def retrieve(self, location):
@@ -18,25 +24,42 @@ class FileStorageBackend(object):
     def get_url(self, location):
         raise NotImplementedError()
 
+
+
 class LocalFileStorageBackend(FileStorageBackend):
     def __init__(self, upload_folder):
         self.upload_folder = upload_folder
         self.program_folder = "./silverflask"
 
-    def store(self, read_pointer, location):
+    def get_store_path(self, location):
         path_tmp = os.path.join(self.upload_folder, location)
-        path = './silverflask' + path_tmp
+        return './silverflask' + path_tmp
+
+
+    def store(self, read_pointer, location):
+        path = self.get_store_path(location)
+        try:
+            folder_path = os.path.dirname(path)
+            os.makedirs(folder_path)
+        except OSError as exception:
+            if exception.errno != errno.EEXIST:
+                raise
+
         if isinstance(read_pointer, FileStorage):
             read_pointer.save(path)
             return self.get_url(location)
 
-        write_pointer = open(path)
-        write_pointer.write(read_pointer.read())
-        write_pointer.close()
+        write_pointer = open(path, 'wb')
+        shutil.copyfileobj(read_pointer, write_pointer)
         return self.get_url(location)
 
+    def exists(self, path):
+        return os.path.exists(path)
+
     def retrieve(self, location):
-        path = os.path.join(self.upload_folder, location)
+        path = self.get_store_path(location)
+        if not self.exists(path):
+            return None
         read_pointer = open(path, 'rb')
         return read_pointer
 
@@ -75,6 +98,13 @@ class FileObject(DataObject, db.Model):
         self.location = ""
         self.name = "DELETED"
 
+    @classmethod
+    def get_cms_form(cls):
+        form = super().get_cms_form()
+        # form.location = fields.FileField("File")
+        return form
+
+
     def as_dict(self):
         d = super().as_dict()
         d.update({
@@ -86,11 +116,87 @@ class FileObject(DataObject, db.Model):
 
 
 class ImageObject(FileObject):
+
     __tablename__ = "imageobject"
     __mapper_args__ = {
         'polymorphic_identity': __tablename__
     }
     # Specifies special operations on Image Files
     id = db.Column(db.Integer, db.ForeignKey('fileobject.id'), primary_key=True)
-    def resize(self):
-        pass
+    __cache_dir__ = "/_resized/"
+
+
+    def resize(self, width=None, height=None, mode='crop', background="white"):
+        def resized_location():
+            orig_url = storage_backend.get_url(self.location)
+            (orig_folder, tail) = os.path.split(orig_url)
+            (fn, ext) = os.path.splitext(tail)
+            new_fn = fn + mode + str(width) + str(height) + ext
+            return orig_folder + self.__cache_dir__ + new_fn
+        rl = resized_location()
+        if storage_backend.exists(rl):
+            print(rl)
+            return rl
+        else:
+            fp = storage_backend.retrieve(self.location)
+            unused_var, ext = os.path.splitext(self.location)
+            resized_img = self._resize(fp, width, height, mode, background)
+            tmp_file_path = "/tmp/" + str(uuid.uuid4()) + ext
+            resized_img.save(tmp_file_path)
+            tmp_file = open(tmp_file_path, "rb")
+            storage_backend.store(tmp_file, rl)
+            return rl
+
+    @staticmethod
+    def _resize(filepointer, width=None, height=None, mode=None, background=None):
+        print("resizing %r %r %r %r" % (filepointer, width, height, mode))
+        img = Image.open(filepointer)
+        orig_width, orig_height = img.size
+
+        width = min(width, orig_width) if width else None
+        height = min(height, orig_height) if height else None
+
+        if not img.mode.lower().startswith('rgb'):
+            img = img.convert('RGBA')
+
+        if width and height:
+
+            fit, crop = sorted([
+                (width, orig_height * width // orig_width),
+                (orig_width * height // orig_height, height)
+            ])
+
+            if mode == 'fit' or mode == 'pad':
+                img = img.resize(fit, Image.ANTIALIAS)
+
+                if mode == 'pad':
+                    pad_color = str(background or 'black')
+                    back = Image.new('RGBA', (width, height), pad_color)
+                    back.paste(img, (
+                        (width - fit[0]) // 2,
+                        (height - fit[1]) // 2
+                    ))
+                    img = back
+
+            elif mode == 'crop':
+                dx = (crop[0] - width) // 2
+                dy = (crop[1] - height) // 2
+                img = img.resize(crop, Image.ANTIALIAS).crop(
+                    (dx, dy, dx + width, dy + height)
+                )
+
+            elif mode == 'reshape' or mode is None:
+                img = img.resize((width, height), Image.ANTIALIAS)
+
+            else:
+                raise ValueError('unsupported mode %r' % mode)
+
+        elif width:
+            height = orig_height * width // orig_width
+            img = img.resize((width, height), Image.ANTIALIAS)
+
+        elif height:
+            width = orig_width * height // orig_height
+            img = img.resize((width, height), Image.ANTIALIAS)
+
+        return img
